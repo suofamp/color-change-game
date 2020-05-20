c8ada1485a02ec28fea195f15c88795b1b561d1b
suofamp <amenosaduti@yahoo.co.jp>
2020-05-20 23:32:59 +0900

    add timer temporary

--- function.js
+++ function.js
@@ -6,9 +6,28 @@ var cColor = ['green','black'];
 var tColor = ['lightpink','black'];
 var tWeight = ['Random','Select'];
 
+var time = 0;
+var mid = 0;
+var now;
+
+var min_time = 0;
+var sec_time = 0;
+
+var count;
+
+var min = $("#min");
+var sec = $("#sec");
+
+var start = $("#start");
+var stop = $("#stop");
+
 function check(){
     if(!$('button').hasClass(cColor[colorFlag])){
-        alert('クリアです！');
+        
+        mid += (new Date() - now)/1000;
+        clearInterval(count);
+        toggle();
+        
         $('.container button').removeClass();
         $('.container button').addClass(color[0]);
         $('#changeColor').css('color',tColor[0]);
@@ -43,6 +62,10 @@ $('.container button').click(function(){
 $('#start').click(function(){
     if(startFlag == 0){
         startFlag = 1;
+        
+        now = new Date(); //現在時刻
+        count = setInterval(counter, 10);
+        
         if(modeFlag == 0){
             for(var i = 0;i < 9;i++){
                 var rand = Math.floor(Math.random() * 2);
@@ -79,4 +102,33 @@ $('#reset').click(function(){
     colorFlag = 0;
     modeFlag = 0;
     startFlag = 0;
+    
+    mid += (new Date() - now)/1000;
+    clearInterval(count);
+    mid = 0;
+    min.html("0");
+    sec.html("00.00");
 });
+
+function counter(){
+
+    time = mid + ((new Date() - now)/1000);
+
+    //60秒経過した時の処理
+    if(time > 60){
+        mid = 0;
+        min_time ++;
+        now = new Date();
+        time = 0;
+        sec.html();
+    }
+
+    //秒数が10秒より小さかったら01, 02のようにする
+    if(time < 10){
+        sec.html("0"+time.toFixed(2));
+    }else{
+        sec.html(time.toFixed(2));
+    }
+    min.html(min_time);
+};
+
